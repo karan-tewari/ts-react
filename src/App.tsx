448a14dae5101c8b3414b50c65b93bf1562f33f6
karan-tewari <ktewari601@gmail.com>
2022-04-12 09:30:03 +0530

    added interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react"
+
+interface PeopleObj {
+  person: {
+    age: number
+    url: string
+    note?: string
+    name: string
+  }[]
+}
+
 function App() {
-  return <div className="App">Hello world</div>
+  const [people, setPeople] = useState<PeopleObj["person"]>([
+    {
+      name: "Koe",
+      age: 32,
+      note: "The best player",
+      url: "",
+    },
+    {
+      name: "Chloe",
+      age: 26,
+      note: "The random entry",
+      url: "",
+    },
+  ])
+
+  people.map((person) => {
+    return person.age
+  })
+
+  return <div>Hello world</div>
 }
 
 export default App
